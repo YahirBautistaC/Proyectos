f83a107f1bd7ded5ab5c3b8088894d8d85904dde
YahirBautistaC <164437616+YahirBautistaC@users.noreply.github.com>
2024-03-22 08:47:36 -0600

    Add files via upload

---
+++ Calentador.C#.Arduino/Calentador_proyecto/Calentador_proyecto/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace Calentador_proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textcrono = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.texttemp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.labelHora = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelcrono = new System.Windows.Forms.Label();
+            this.labeltemp = new System.Windows.Forms.Label();
+            this.hora = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 472);
+            this.panel1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chart1.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Top;
+            this.chart1.BackSecondaryColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 3);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series1.BorderColor = System.Drawing.Color.DarkSeaGreen;
+            series1.BorderWidth = 9;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.MarkerBorderColor = System.Drawing.Color.Black;
+            series1.MarkerBorderWidth = 5;
+            series1.MarkerColor = System.Drawing.Color.Black;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series1.Name = "°C";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Black;
+            series2.Legend = "Legend1";
+            series2.Name = "0";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(769, 459);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.textcrono);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.texttemp);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(793, 120);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(221, 213);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(23, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Comprobar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(127, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "seg";
+            // 
+            // textcrono
+            // 
+            this.textcrono.BackColor = System.Drawing.Color.DimGray;
+            this.textcrono.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textcrono.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textcrono.ForeColor = System.Drawing.SystemColors.Window;
+            this.textcrono.Location = new System.Drawing.Point(61, 119);
+            this.textcrono.Multiline = true;
+            this.textcrono.Name = "textcrono";
+            this.textcrono.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textcrono.Size = new System.Drawing.Size(60, 25);
+            this.textcrono.TabIndex = 5;
+            this.textcrono.Text = "\r\n";
+            this.textcrono.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textcrono.TextChanged += new System.EventHandler(this.textcrono_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(20, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Inserte tiempo deseado";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(114, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Encender";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(127, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "°C";
+            // 
+            // texttemp
+            // 
+            this.texttemp.BackColor = System.Drawing.Color.DimGray;
+            this.texttemp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.texttemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.texttemp.ForeColor = System.Drawing.SystemColors.Window;
+            this.texttemp.Location = new System.Drawing.Point(61, 53);
+            this.texttemp.Multiline = true;
+            this.texttemp.Name = "texttemp";
+            this.texttemp.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.texttemp.Size = new System.Drawing.Size(60, 25);
+            this.texttemp.TabIndex = 1;
+            this.texttemp.Text = "\r\n";
+            this.texttemp.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.texttemp.TextChanged += new System.EventHandler(this.texttemp_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(8, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inserte temperatura deseada";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.panel4.Controls.Add(this.labelHora);
+            this.panel4.Location = new System.Drawing.Point(793, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(221, 102);
+            this.panel4.TabIndex = 1;
+            // 
+            // labelHora
+            // 
+            this.labelHora.AutoSize = true;
+            this.labelHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHora.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelHora.Location = new System.Drawing.Point(-5, 17);
+            this.labelHora.Name = "labelHora";
+            this.labelHora.Size = new System.Drawing.Size(159, 63);
+            this.labelHora.TabIndex = 0;
+            this.labelHora.Text = "h:m:s";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Controls.Add(this.labelcrono);
+            this.panel5.Controls.Add(this.labeltemp);
+            this.panel5.Location = new System.Drawing.Point(793, 339);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(221, 145);
+            this.panel5.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Yellow;
+            this.label5.Location = new System.Drawing.Point(142, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 54);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "°C";
+            // 
+            // labelcrono
+            // 
+            this.labelcrono.AutoSize = true;
+            this.labelcrono.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelcrono.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.labelcrono.Location = new System.Drawing.Point(62, 81);
+            this.labelcrono.Name = "labelcrono";
+            this.labelcrono.Size = new System.Drawing.Size(101, 54);
+            this.labelcrono.TabIndex = 1;
+            this.labelcrono.Text = "000";
+            // 
+            // labeltemp
+            // 
+            this.labeltemp.AutoSize = true;
+            this.labeltemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeltemp.ForeColor = System.Drawing.Color.Yellow;
+            this.labeltemp.Location = new System.Drawing.Point(-5, 18);
+            this.labeltemp.Name = "labeltemp";
+            this.labeltemp.Size = new System.Drawing.Size(162, 63);
+            this.labeltemp.TabIndex = 0;
+            this.labeltemp.Text = "00.00";
+            // 
+            // hora
+            // 
+            this.hora.Enabled = true;
+            this.hora.Tick += new System.EventHandler(this.hora_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(1026, 493);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calentador";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox texttemp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelHora;
+        private System.Windows.Forms.Timer hora;
+        private System.Windows.Forms.Label labeltemp;
+        private System.Windows.Forms.Label labelcrono;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textcrono;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Calentador.C#.Arduino/Calentador_proyecto/Calentador_proyecto/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Calentador_proyecto
+{
+    public partial class Form1 : Form
+    {
+        private readonly System.IO.Ports.SerialPort Port = new System.IO.Ports.SerialPort("COM6", 9600) { ReadTimeout = 1000 };
+        private bool IsClose;
+        private int txtcr, txttp, temp, tiempoRestante, tempFromArduino;
+        private readonly object tempLock = new object();
+        private int temp1;
+
+        private double temperatura;  // Cambié el tipo de la variable a double
+        private double tempEntrada;
+
+        private int intervaloContador = 0;
+
+        private readonly System.Windows.Forms.Timer temporizador = new System.Windows.Forms.Timer { Interval = 1000 };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                Port.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al abrir el puerto serial: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            temporizador.Tick += Temporizador_Tick;
+        }
+
+        private void hora_Tick(object sender, EventArgs e) => labelHora.Text = DateTime.Now.ToString("hh:mm:ss");
+
+        private void Temporizador_Tick(object sender, EventArgs e)
+        {
+            intervaloContador++;
+            if (intervaloContador % 10 == 0) // Aquí esta el intervalo de tiempo de cada cuantos segundos agrega datos
+            {
+                // Agrega el valor actual al gráfico
+                AgregarValorAlGrafico();
+            }
+
+            if (tiempoRestante > 0)
+            {
+                tiempoRestante--;
+                labelcrono.Text = $"{tiempoRestante}";
+
+                // Llama a VerificarTemperatura cada segundo
+                VerificarTemperatura();
+            }
+            else
+            {
+                temporizador.Stop();
+                MessageBox.Show("Temporizador finalizado.");
+
+                txtcr = txttp = temp = 0;
+                Port.Write("A");
+
+                labeltemp.Invoke(new MethodInvoker(() => labeltemp.Text = "0"));
+
+                chart1.Series[0].Points.Clear();
+            }
+        }
+
+        private void AgregarValorAlGrafico()
+        {
+            chart1.Series[0].Points.AddXY(tiempoRestante, temperatura);
+        }
+
+
+        // Agrega un método para verificar la temperatura y escribir en el puerto si es necesario
+        private void VerificarTemperatura()
+        {
+            if (temperatura >= tempEntrada)
+            {
+                Port.Write("A");
+            }
+            else if (temperatura < tempEntrada)
+            {
+                Port.Write("E");
+            }
+        }
+
+        private void EscucharSerial()
+        {
+            while (!IsClose)
+            {
+                try
+                {
+                    string cadena = Port.ReadLine();
+                    if (double.TryParse(cadena,out double tempValue))
+                    {
+                        lock (tempLock)
+                        {
+                            temperatura = tempValue;
+                        }
+
+                        labeltemp.Invoke(new MethodInvoker(delegate
+                        {
+                            labeltemp.Text = cadena;
+                        }));
+                    }
+                    else
+                    {
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Puedes manejar el error de manera adecuada, por ejemplo, mostrando un mensaje de error.
+                    // MessageBox.Show("Error al leer el puerto serial: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) => new Thread(EscucharSerial).Start();
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            IsClose = true;
+            if (Port.IsOpen)
+            {
+                // Escribir "A" en el puerto antes de cerrarlo
+                Port.Write("A");
+                Port.Close();
+            }
+        }
+
+        private void textcrono_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(textcrono.Text, out int valor))
+            {
+                chart1.Series[0].Points.Clear();
+                chart1.ChartAreas[0].AxisY.Minimum = -55;
+                chart1.ChartAreas[0].AxisY.Maximum = 125;
+                chart1.ChartAreas[0].AxisY.Interval = 15;
+
+                for (int i = 0; i <= valor; i += 15) // Aquí esta el intervalo de eje x
+                {
+                    chart1.Series[1].Points.AddXY(i, 0);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un valor numérico válido.");
+            }
+        }
+
+        private void texttemp_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(texttemp.Text, out double valorTemp))
+            {
+                tempEntrada = valorTemp;
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un valor numérico válido para la temperatura.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtcr = int.Parse(textcrono.Text);
+                txttp = int.Parse(texttemp.Text);
+
+                MessageBox.Show($"Valores guardados: \n{tempEntrada} °C\n{txtcr} seg");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Error de formato en la entrada. Asegúrate de ingresar números válidos.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tiempoRestante = int.Parse(textcrono.Text);
+                labelcrono.Text = $"{tiempoRestante}";
+                temporizador.Start();
+                Port.Write("E");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Error de formato en la entrada. Asegúrate de ingresar números válidos.");
+            }
+        }
+    }
+}
